2a8a04fdf4e2c38d50a583bb2a6cfea3c22e5076
Pablo Bezerra <pablobzerrads@gmail.com>
2025-07-20 07:23:03 -0300

    Update WSClientOkHttp.java

--- Client/WSClientOkHttp.java
+++ Client/WSClientOkHttp.java
@@ -1,9 +1,9 @@
 package com.websocket.client.utils;
 
-import com.websocket.client.MainActivity;
-import com.websocket.client.R;
+//import com.MainActivity;
+//import com.R;
 
-import android.widget.TextView;
+//import android.widget.TextView;
 
 import okhttp3.*;
 
@@ -11,12 +11,13 @@ import okhttp3.*;
 public class WSClientOkHttp {
     
     private WebSocket ws;
-    private MainActivity ctx;
+    //private MainActivity ctx;
     
-    
+    /*
     public WSClientOkHttp(MainActivity ctx){
         this.ctx = ctx;
     }
+    */
     
     
     public void connect(String host, String token) {
@@ -31,22 +32,30 @@ public class WSClientOkHttp {
             public void onOpen(WebSocket ws, Response r) {
                super.onOpen(ws, r);
                ws.send("ping");
-               TextView status = ctx.findViewById(R.id.status);
+               /*
+                TextView status = ctx.findViewById(R.id.status);
                status.setText("Conectado!");
+               */
             }
             
             @Override
             public void onMessage(WebSocket ws, String data) {
                 super.onMessage(ws, data);
+
+                /*
                 TextView msg = ctx.findViewById(R.id.msg);
                 msg.setText(data);
+                */
             }
             
             @Override
             public void onClosing(WebSocket ws, int code, String r) {
                 super.onClosing(ws, code, r);
+
+                /*
                 TextView status = ctx.findViewById(R.id.status);
                 status.setText("Code: " + code + r);
+                */
             }
             
             
